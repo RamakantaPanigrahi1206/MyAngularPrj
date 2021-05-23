fb329a5a16a49b4432271693f04fc93c26fdf35c
RamakantaPanigrahi1206 <ramakanta.panigrahi1206@gmail.com>
2021-05-23 23:42:28 +0530

    My first Angular

---
+++ my-second-prj/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-second-prj';
+  constructor(){
+    // setTimeout(() => {
+    //   this.title = "Canged title" ;
+    // }, 2000 );
+    
+  }
+  
+}
